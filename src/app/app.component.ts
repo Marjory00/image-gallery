a935dde177116c5982dab56b4497561efcb11cd2
Marjory <samaterra@hotmail.com>
2022-12-18 10:03:20 -0500

    fixing get.data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 
 export class AppComponent {
   title = 'image-gallery';
-  private data:any = [] // creates an empty array where images from API will be stored.
+  public data:any = [] // creates an empty array where images from API will be stored.
   constructor(private http: HttpClient) {
 
   }
@@ -19,8 +19,8 @@ export class AppComponent {
 
   getData() { // the URL get passed into the this.http.get() method, which sends a GET request to the URL.
 
-    const url ='https://jsonplaceholder.typicode.com/photos?albulmId-1'
-    this.http.get(url).subscribe((res)=> { /* subscribe() method listen and awaits for response,
+    const url ='https://jsonplaceholder.typicode.com/photos?albumId=1'
+    this.http.get(url).subscribe((res)=>{ /* subscribe() method listen and awaits for response,
       then response data, res, once available is saved in the data array created. */
       this.data = res
       console.log(this.data)
